844b6466c9de6d7ccdc06342512d3b16b9f46781
Victor Hugo <victorhugomarquezin@gmail.com>
2023-09-12 22:42:19 -0300

    adicionar libray para envio de emails, e envio de token

---
+++ application/config/email.php
@@ -0,0 +1,16 @@
+<?php
+
+ //Configuração mail
+ $config['smtp_host'] = 'mail.petroecol.eco.br';
+ $config['smtp_port'] = '587';
+ $config['smtp_user'] = 'contato@petroecol.eco.br';
+ $config['smtp_pass'] = '@@123contato';
+ $config['protocol'] = 'smtp';
+ $config['wordwrap'] = TRUE;
+ $config['validate'] = TRUE;
+ $config['mailtype'] = 'html';
+ $config['charset'] = 'utf-8';
+ $config['newline'] = "\r\n";
+
+
+?>
