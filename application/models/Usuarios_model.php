6c86029ae6113f9c8b9d87e7545bc8ca24277b13
Victor Hugo <victorhugomarquezin@gmail.com>
2023-09-14 10:36:43 -0300

    backend troca de senha - redefinicao de senha

--- application/models/Usuarios_model.php
+++ application/models/Usuarios_model.php
@@ -3,7 +3,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Usuarios_model extends CI_Model {
 	
-	public function recebeUsuarios(){
+	public function recebeUsuarios()
+    {
 		
 		$this->db->order_by('nome', 'DESC');  
         $query = $this->db->get('ci_usuarios');
@@ -12,7 +13,23 @@ class Usuarios_model extends CI_Model {
 		
     }
 
-	public function recebeUsuarioEmail($email){
+    public function exibeUsuario($id) 
+    {
+        $this->db->where('id', $id);
+        $query = $this->db->get('ci_usuarios');
+			
+        return $query->row_array();
+    }
+
+    public function exibeUsuarios()
+    {
+        $query = $this->db->get('ci_usuarios');
+			
+        return $query->result_array();
+    }
+
+	public function recebeUsuarioEmail($email)
+    {
 		
 		$this->db->where('email', $email);
         $query = $this->db->get('ci_usuarios');
@@ -24,5 +41,12 @@ class Usuarios_model extends CI_Model {
     public function insereUsuario($dados)
     {
         $this->db->insert('ci_usuarios', $dados);
+    }
+
+    public function editaUsuario($id, $dados)
+    {
+        $this->db->where('id', $id);
+        $this->db->update('ci_usuarios', $dados);
+        return $this->db->affected_rows() > 0;
     }
 }
