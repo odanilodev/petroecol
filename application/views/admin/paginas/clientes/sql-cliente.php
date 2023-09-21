5ce2e9c3f057e883d019ed6bad2f946c2d57518a
Victor Hugo <victorhugomarquezin@gmail.com>
2023-09-21 16:41:41 -0300

    atualizacao sql

--- application/views/admin/paginas/clientes/sql-cliente.php
+++ application/views/admin/paginas/clientes/sql-cliente.php
@@ -4,26 +4,22 @@ error_reporting(0);
 ini_set("display_errors", 0 );
 ini_set("memory_limit", "-1");
 set_time_limit(0);
-       
-    $handle = fopen("assets/data/clientes2.csv", "r");
+    
+    $handle = fopen("docs/petroecol.csv", "r");
 
-    echo "INSERT INTO ci_clientes (id, codigo, nome, razao_social, email, telefone, tipo_negocio, grupo_negocio, rua, numero, cep, bairro, complemento, cidade, estado, pais, cnpj, inscricao_estadual, observacao, data_criacao, dia_pagamento, status, data_inativo, atendimentos_agendados, atendimentos_atrasados, atendimentos_finalizados, frequencia_coleta)
-        VALUES";
-    
-    
+    echo "INSERT INTO ci_clientes (id_antigo, codigo, nome_cliente, razao_social, email, telefone, tipo_negocio, grupo_negocio, rua, numero, cep, bairro, complemento, cidade, estado, pais, cnpj, inscricao_estadual, observacao, data_criacao, dia_pagamento, status, data_inativo, atendimentos_agendados, atendimentos_atrasados,atendimentos_finalizados, frequencia_coleta) 
+        VALUES <br>";
+            
     $row = 0;
     while ($v = fgetcsv($handle, 1000, ";")) {
-        if ($row++ == 0) { // pula primeira linha
+        if ($row++ == 0) { // pula primeira linha 
             continue;
         }
 
-
-        echo "($v[1], \"$v[2]\", \"$v[3]\", \"$v[4]\", \"$v[5]\", \"$v[6]\", \"$v[12]\", \"$v[7]\", \"$v[8]\", \"$v[9]\", \"$v[10]\", \"$v[11]\", $v[0],0),<br>";
-
+        echo "($v[0], \"$v[1]\", \"$v[2]\", \"$v[3]\", \"$v[4]\", \"$v[5]\", \"$v[6]\", \"$v[7]\", \"$v[8]\", \"$v[9]\", \"$v[10]\", \"$v[11]\", \"$v[12]\", \"$v[13]\", \"$v[14]\", \"$v[15]\", \"$v[16]\", \"$v[17]\", \"$v[18]\", \"$v[19]\", \"$v[20]\", \"$v[21]\", \"$v[22]\", \"$v[23]\", \"$v[24]\", \"$v[25]\", \"$v[26]\", \"$v[27]\"),<br>";
         
     }
-   
-   
-        fclose($handle);
-
+    fclose($handle);
+		
+	
 ?>
