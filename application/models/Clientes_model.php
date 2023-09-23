ec4745def119268298f753069b2a6d0ca19e0d46
Danilo <contato@centrodainteligencia.com.br>
2023-09-23 09:51:07 -0300

    Criar log para o sistema

--- application/models/Clientes_model.php
+++ application/models/Clientes_model.php
@@ -4,6 +4,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Clientes_model extends CI_Model {
 
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Log_model');
+    }
+
     public function recebeClientes()
     {
         $this->db->order_by('nome', 'DESC');
@@ -22,17 +28,14 @@ class Clientes_model extends CI_Model {
         return $query->row_array();
     }
 
-    public function exibeClientes()
-    {
-        $query = $this->db->get('ci_clientes');
-        $this->db->where('id_empresa', $this->session->userdata('id_empresa'));
-
-        return $query->result_array();
-    }
-
     public function insereCliente($dados)
     {
         $this->db->insert('ci_clientes', $dados);
+
+        if ($this->db->affected_rows()) {
+            $this->Log_model->insereLog($this->db->insert_id());
+        }
+
         return $this->db->affected_rows() > 0;
         
     }
@@ -42,6 +45,11 @@ class Clientes_model extends CI_Model {
         $this->db->where('id', $id);
         $this->db->where('id_empresa', $this->session->userdata('id_empresa'));
         $this->db->update('ci_clientes', $dados);
+
+        if ($this->db->affected_rows()) {
+            $this->Log_model->insereLog($id);
+        }
+
         return $this->db->affected_rows() > 0;
     }
 
@@ -50,6 +58,10 @@ class Clientes_model extends CI_Model {
         $this->db->where('id', $id);
         $this->db->where('id_empresa', $this->session->userdata('id_empresa'));
         $this->db->delete('ci_clientes');
+
+        if ($this->db->affected_rows()) {
+            $this->Log_model->insereLog($id);
+        }
 
         return $this->db->affected_rows() > 0;
     }
