c3570dd689770a218d7961fbac9868cfbd468d46
Danilo <contato@centrodainteligencia.com.br>
2023-09-25 15:29:54 -0300

    model etiqueta cliente

--- application/models/EtiquetaCliente_model.php
+++ application/models/EtiquetaCliente_model.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Usuarios_model extends CI_Model
+class EtiquetaCliente_model extends CI_Model
 {
 
     public function __construct()
@@ -10,36 +10,38 @@ class Usuarios_model extends CI_Model
         $this->load->model('Log_model');
     }
 
-    public function recebeUsuarios()
+    public function recebeEtiquetasClientes()
     {
-        $this->db->order_by('nome', 'DESC');
-        $this->db->where('id_empresa', $this->session->userdata('id_empresa'));
-        $query = $this->db->get('ci_usuarios');
+        $this->db->select('EC.*, C.nome, E.nome');
+        $this->db->from('ci_etiqueta_cliente EC');
+        $this->db->join('ci_clientes C', 'EC.id_cliente = C.id', 'inner');
+        $this->db->join('ci_etiquetas E', 'EC.id_etiqueta = E.id_etiqueta', 'inner');
+        $this->db->where('EC.id_empresa', $this->session->userdata('id_empresa'));
+        $this->db->where('C.id_empresa', $this->session->userdata('id_empresa'));
+        $this->db->where('E.id_empresa', $this->session->userdata('id_empresa'));
+        $query = $this->db->get();
 
         return $query->result_array();
     }
 
-    public function recebeUsuario($id)
+    public function recebeEtiquetaCliente($id)
     {
-        $this->db->where('id', $id);
-        $this->db->where('id_empresa', $this->session->userdata('id_empresa'));
-        $query = $this->db->get('ci_usuarios');
+        $this->db->select('EC.*, C.nome, E.nome');
+        $this->db->from('ci_etiqueta_cliente EC');
+        $this->db->join('ci_clientes C', 'EC.id_cliente = C.id', 'inner');
+        $this->db->join('ci_etiquetas E', 'EC.id_etiqueta = E.id_etiqueta', 'inner');
+        $this->db->where('EC.id_empresa', $this->session->userdata('id_empresa'));
+        $this->db->where('C.id_empresa', $this->session->userdata('id_empresa'));
+        $this->db->where('E.id_empresa', $this->session->userdata('id_empresa'));
+        $this->db->where('EC.id', $id);
+        $query = $this->db->get();
 
         return $query->row_array();
     }
 
-    public function recebeUsuarioEmail($email)
+    public function insereEtiquetaCliente($dados)
     {
-
-        $this->db->where('email', $email);
-        $query = $this->db->get('ci_usuarios');
-
-        return $query->row_array();
-    }
-
-    public function insereUsuario($dados)
-    {
-        $this->db->insert('ci_usuarios', $dados);
+        $this->db->insert('ci_etiqueta_cliente', $dados);
 
         if ($this->db->affected_rows()) {
             $this->Log_model->insereLog($this->db->insert_id());
@@ -48,11 +50,11 @@ class Usuarios_model extends CI_Model
         return $this->db->affected_rows() > 0;
     }
 
-    public function editaUsuario($id, $dados)
+    public function editaEtiquetaCliente($id, $dados)
     {
         $this->db->where('id', $id);
         $this->db->where('id_empresa', $this->session->userdata('id_empresa'));
-        $this->db->update('ci_usuarios', $dados);
+        $this->db->update('ci_etiqueta_cliente', $dados);
 
         if ($this->db->affected_rows()) {
             $this->Log_model->insereLog($id);
@@ -61,30 +63,11 @@ class Usuarios_model extends CI_Model
         return $this->db->affected_rows() > 0;
     }
 
-    public function verificaSenhaAntiga($id, $senhaAntiga)
+    public function deletaEtiquetaCliente($id)
     {
         $this->db->where('id', $id);
         $this->db->where('id_empresa', $this->session->userdata('id_empresa'));
-        $this->db->where('senha', $senhaAntiga);
-        $query = $this->db->get('ci_usuarios');
-
-        return $query->row_array();
-    }
-
-    public function imagemAntiga($id)
-    {
-        $this->db->where('id', $id);
-        $this->db->where('id_empresa', $this->session->userdata('id_empresa'));
-        $query = $this->db->get('ci_usuarios');
-
-        return $query->row_array();
-    }
-
-    public function deletaUsuario($id)
-    {
-        $this->db->where('id', $id);
-        $this->db->where('id_empresa', $this->session->userdata('id_empresa'));
-        $this->db->delete('ci_usuarios');
+        $this->db->delete('ci_etiqueta_cliente');
 
         if ($this->db->affected_rows()) {
             $this->Log_model->insereLog($id);
