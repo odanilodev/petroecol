1062e7f07df909d1c47d8bd9dde6002ffd22067b
Victor Hugo <victorhugomarquezin@gmail.com>
2023-09-26 12:52:33 -0300

    Correcao hora minuto e segundo mode l

--- application/models/Etiquetas_model.php
+++ application/models/Etiquetas_model.php
@@ -38,7 +38,7 @@ class Etiquetas_model extends CI_Model
 
     public function insereEtiqueta($dados)
     {
-        $dados['criado_em'] = date('Y-m-d H:m:s');
+        $dados['criado_em'] = date('Y-m-d H:i:s');
 
         $this->db->insert('ci_etiquetas', $dados);
 
@@ -51,7 +51,7 @@ class Etiquetas_model extends CI_Model
 
     public function editaEtiqueta($id, $dados)
     {
-        $dados['editado_em'] = date('Y-m-d H:m:s');
+        $dados['editado_em'] = date('Y-m-d H:i:s');
 
         $this->db->where('id', $id);
         $this->db->where('id_empresa', $this->session->userdata('id_empresa'));
