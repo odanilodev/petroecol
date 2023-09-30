6c40b3d868af52a66ab4ce2f1e55bac39f153253
oluisfelipedev <lipe@centrodainteligencia.com.br>
2023-09-30 19:28:59 -0300

    avisos de recuperação de senha

--- application/views/admin/login/login.php
+++ application/views/admin/login/login.php
@@ -102,15 +102,6 @@
                   </div>
                   <!--/.bg-holder-->
 
-                  <div class="position-relative px-4 px-lg-7 pt-7 pb-7 pb-sm-5 text-center text-md-start pb-lg-7 pb-md-7">
-                    <h3 class="mb-3 text-black fs-1">Phoenix Authentication</h3>
-                    <p class="text-700">Give yourself some hassle-free development process with the uniqueness of Phoenix!</p>
-                    <ul class="list-unstyled mb-0 w-max-content w-md-auto mx-auto">
-                      <li class="d-flex align-items-center"><span class="uil uil-check-circle text-success me-2"></span><span class="text-700 fw-semi-bold">Fast</span></li>
-                      <li class="d-flex align-items-center"><span class="uil uil-check-circle text-success me-2"></span><span class="text-700 fw-semi-bold">Simple</span></li>
-                      <li class="d-flex align-items-center"><span class="uil uil-check-circle text-success me-2"></span><span class="text-700 fw-semi-bold">Responsive</span></li>
-                    </ul>
-                  </div>
                   <div class="position-relative z-index--1 mb-6 d-none d-md-block text-center mt-md-15"><img class="auth-title-box-img d-dark-none" src="<?= base_url() ?>assets/img/spot-illustrations/auth.png" alt="" /><img class="auth-title-box-img d-light-none" src="<?= base_url() ?>assets/img/spot-illustrations/auth-dark.png" alt="" /></div>
                 </div>
                 <div class="col mx-auto">
