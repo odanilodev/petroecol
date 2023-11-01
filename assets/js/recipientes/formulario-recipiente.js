db33473df6ce6e20597db1acdc68632728aa8e25
Guilherme Prado <guilherme6788@hotmail.com>
2023-11-01 15:15:26 -0300

    Correção do aviso de retorno

--- assets/js/recipientes/formulario-recipiente.js
+++ assets/js/recipientes/formulario-recipiente.js
@@ -69,7 +69,7 @@ const deletarRecipiente = (id) => {
 
     Swal.fire({
         title: 'Você tem certeza?',
-        text: "Esta ação não porerá ser revertida",
+        text: "Esta ação não poderá ser revertida",
         icon: 'warning',
         showCancelButton: true,
         confirmButtonColor: '#3085d6',
