81633c0af430b80fdbc1e72843784ca66b5268b7
Guilherme Prado <guilherme6788@hotmail.com>
2023-11-27 19:10:02 -0300

    Alteração no prompt de forma de pagamento e função do menu LOG

--- application/controllers/Clientes.php
+++ application/controllers/Clientes.php
@@ -146,8 +146,6 @@ class Clientes extends CI_Controller
         // Coloca os arrays em uma única variável
         $dados = array_merge($dadosEmpresa, $dadosEndereco, $dadosResponsavel);
 
-        $dataHoraAtual = date('Y-m-d H:i:s');
-
         $dados['id_empresa'] = $this->session->userdata('id_empresa');
 
         $retorno = $id ? $this->Clientes_model->editaCliente($id, $dados) : $this->Clientes_model->insereCliente($dados); // se tiver ID edita se não INSERE
