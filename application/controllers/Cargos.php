5a3d1a3dd287a4963259138f579a22cde7d81efe
Guilherme Prado <guilherme6788@hotmail.com>
2023-12-06 18:49:42 -0300

    Formulario para cadastro de funcao

---
+++ application/controllers/Cargos.php
@@ -0,0 +1,110 @@
+	<?php
+	defined('BASEPATH') or exit('No direct script access allowed');
+
+	class Cargos extends CI_Controller
+	{
+		public function __construct()
+		{
+			parent::__construct();
+
+			// INICIO controle sessão
+			$this->load->library('Controle_sessao');
+			$res = $this->controle_sessao->controle();
+			if ($res == 'erro') {
+				redirect('login/erro', 'refresh');
+			}
+			// FIM controle sessão
+			$this->load->model('Cargos_model');
+		}
+
+		public function index()
+		{
+			// scripts padrão
+			$scriptsPadraoHead = scriptsPadraoHead();
+			$scriptsPadraoFooter = scriptsPadraoFooter();
+
+			// scripts para Cargos
+			$scriptsCargosFooter = scriptsCargosFooter();
+
+			add_scripts('header', array_merge($scriptsPadraoHead));
+			add_scripts('footer', array_merge($scriptsPadraoFooter, $scriptsCargosFooter));
+
+			$data['cargos'] = $this->Cargos_model->recebeCargos();
+
+			$this->load->view('admin/includes/painel/cabecalho', $data);
+			$this->load->view('admin/paginas/cargos/cargos');
+			$this->load->view('admin/includes/painel/rodape');
+		}
+
+		public function formulario()
+		{
+			// scripts padrão
+			$scriptsPadraoHead = scriptsPadraoHead();
+			$scriptsPadraoFooter = scriptsPadraoFooter();
+
+			// scripts para cargos
+			$scriptsCargosFooter = scriptsCargosFooter();
+
+			add_scripts('header', array_merge($scriptsPadraoHead));
+			add_scripts('footer', array_merge($scriptsPadraoFooter, $scriptsCargosFooter));
+
+			$id = $this->uri->segment(3);
+
+			$data['cargos'] = $this->Cargos_model->recebeCargo($id);
+
+			$this->load->view('admin/includes/painel/cabecalho', $data);
+			$this->load->view('admin/paginas/cargos/cadastra-cargos');
+			$this->load->view('admin/includes/painel/rodape');
+		}
+
+		public function cadastraCargos()
+		{
+			$id = $this->input->post('id');
+
+			$nome = $this->input->post('nome');
+			$dados['nome'] = mb_convert_case($nome, MB_CASE_TITLE, 'UTF-8');
+			$dados['id_empresa'] = $this->session->userdata('id_empresa');
+			$dados['responsavel_agendamento'] = $this->input->post('responsavelAgendamento');
+
+			$cargo = $this->Cargos_model->recebeCargoNome($dados['nome']); // verifica se já existe o cargo
+
+			// Verifica se o cargo já existe e se não é o cargo que está sendo editada
+			if ($cargo && $cargo['id'] != $id) {
+
+				$response = array(
+					'success' => false,
+					'message' => "Este cargo já existe! Tente cadastrar um diferente."
+				);
+
+				return $this->output->set_content_type('application/json')->set_output(json_encode($response));
+			}
+
+			$retorno = $id ? $this->Cargos_model->editaCargo($id, $dados) : $this->Cargos_model->insereCargo($dados); // se tiver ID edita se não INSERE
+
+			if ($retorno) { // inseriu ou editou
+
+				$response = array(
+					'success' => true,
+					'message' => $id ? 'Cargo editado com sucesso!' : 'Cargo cadastrado com sucesso!'
+				);
+			} else { // erro ao inserir ou editar
+
+				$response = array(
+					'success' => false,
+					'message' => $id ? "Erro ao editar o cargo!" : "Erro ao cadastrar o cargo!"
+				);
+			}
+
+			return $this->output->set_content_type('application/json')->set_output(json_encode($response));
+		}
+
+
+		public function deletaCargos()
+		{
+			$id = $this->input->post('id');
+
+			$this->Cargos_model->deletaCargo($id);
+
+			redirect('cargos');
+		}
+	}
