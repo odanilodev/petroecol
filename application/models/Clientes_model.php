2eb6d876c8a7b0415fd22aaf122da79c2716ae0d
Victor Hugo Prado <97893980+ovitindev@users.noreply.github.com>
2023-12-23 11:21:34 -0300

    Adicionar logica para agendamento automatico, library para agendar po… (#156)
    * Adicionar logica para agendamento automatico, library para agendar por frequencia
    
    * Correcao de logica para alterar status do agendamento para 2
    
    * Simplificando codido
    
    * Otimizar codigo com ajustes em algumas regras
    
    ---------
    
    Co-authored-by: Danilo <contato@centrodainteligencia.com.br>

--- application/models/Clientes_model.php
+++ application/models/Clientes_model.php
@@ -99,6 +99,19 @@ class Clientes_model extends CI_Model
         return $query->row_array();
     }
 
+    public function recebeClienteFrequenciaColeta($id_cliente)
+    {
+        $this->db->select('F.dia');
+        $this->db->from('ci_clientes C');
+        $this->db->join('ci_frequencia_coleta F', 'C.id_frequencia_coleta = F.id', 'left');
+        $this->db->where('C.id', $id_cliente);
+        $this->db->where('C.id_empresa', $this->session->userdata('id_empresa'));
+        $query = $this->db->get();
+
+        return $query->row_array();
+    }
+
+
     //Recebe clientes com varios Ids selecionados
     public function recebeClientesIds($ids)
     {
