59fc4b77f8978720d7d3f8e2324a9e7c72071571
Guilherme Prado <guilherme6788@hotmail.com>
2023-12-30 11:29:26 +0000

    cadastra-cargos arrumado

--- application/views/admin/paginas/cargos/cadastra-cargos.php
+++ application/views/admin/paginas/cargos/cadastra-cargos.php
@@ -28,8 +28,8 @@
 
                                                 <div class="col-md-12 mb-3">
                                                     <label class="form-label">Nome</label>
-                                                    <input class="form-control input-nome aviso-obrigatorio" type="text" placeholder="Nome do Cargo" value="<?= $cargo['nome'] ?? ''; ?>">
-                                                    <div class="campo-obrigatorio d-none">Preencha este campo</div>
+                                                    <input class="form-control input-cargo " type="text" placeholder="Nome do Cargo" value="<?= $cargo['nome'] ?? ''; ?>">
+                                                    <div class="campo-obrigatorio d-none aviso-obrigatorio">Preencha este campo</div>
                                                 </div>
 
                                                 <div class="col-md-12 mb-3">
