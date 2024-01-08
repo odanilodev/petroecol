4e4a9a221a19a83d99c29cae9fe36fcd6370cee1
Luis Felipe Lopes <99437129+oluisfelipedev@users.noreply.github.com>
2024-01-08 20:52:44 -0300

    adicionar campo para selecionar a orientação do certificado (#203)

--- application/views/admin/paginas/certificados/cadastra-certificado.php
+++ application/views/admin/paginas/certificados/cadastra-certificado.php
@@ -27,51 +27,63 @@
                         <input type="hidden" class="input-id" value="<?= $certificado['id'] ?? ''; ?>">
 
                         <div class="col-md-6 mb-3">
-                          <label class="form-label"><?=chave('modelo')?></label>
+                          <label class="form-label"><?= chave('modelo') ?></label>
                           <input class="form-control input-modelo input-obrigatorio" type="text" placeholder="Nome do Modelo" value="<?= $certificado['modelo'] ?? ''; ?>">
                           <div class="aviso-obrigatorio d-none">Preencha este campo</div>
                         </div>
 
                         <div class="col-md-6 mb-3">
-                          <label class="form-label"><?=chave('titulo')?></label>
+                          <label class="form-label"><?= chave('titulo') ?></label>
                           <input class="form-control input-titulo input-obrigatorio" type="text" placeholder="Titulo do Certificado" value="<?= $certificado['titulo'] ?? ''; ?>">
                           <div class="aviso-obrigatorio d-none">Preencha este campo</div>
                         </div>
 
-                        <div class="col-md-6 mb-3">
-                          <label class="form-label"><?=chave('descricao')?></label>
+                        <div class="col-md-4 mb-3">
+                          <label class="form-label"><?= chave('descricao') ?></label>
                           <input class="form-control input-descricao" type="text" placeholder="Descrição do Certificado" value="<?= $certificado['descricao'] ?? ''; ?>">
-                          
+
                         </div>
 
-                        <div class="col-md-6 mb-3">
-                          <label class="form-label"><?=chave('declaracao')?></label>
+                        <div class="col-md-4 mb-3">
+                          <label class="form-label"><?= chave('declaracao') ?></label>
                           <input class="form-control input-declaracao" type="text" placeholder="Declaração do Certificado" value="<?= $certificado['declaracao'] ?? ''; ?>">
-                          
+
+                        </div>
+
+                        <div class="col-md-4 mb-3">
+                        <label class="form-label">Orientação do certificado</label>
+
+                          <select class="select-validation select-orientacao" required>
+                            <option selected disabled value=''>Selecione</option>
+
+                            <option value="horizontal" <?= (isset($certificado['orientacao']) && $certificado['orientacao'] == 'horizontal') ? 'selected' : ''; ?>>Horizontal</option>
+                            <option value="vertical" <?= (isset($certificado['orientacao']) && $certificado['orientacao'] == 'vertical') ? 'selected' : ''; ?>>Vertical</option>
+
+                          </select>
                         </div>
 
                         <div class="col-md-3 mb-3">
-                          <label class="form-label" title="Mínimo 200px x 100px em formato JPG"><?=chave('logo')?> <small>(200px X 100px JPG)</small> </label>
+                          <label class="form-label" title="Mínimo 200px x 100px em formato JPG"><?= chave('logo') ?> <small>(200px X 100px JPG)</small> </label>
                           <input class="form-control input-logo <?= !$this->uri->segment(3) ? "input-obrigatorio" : "" ?>" type="file" value="<?= $certificado['logo'] ?? ''; ?>">
-                          
+
                         </div>
 
                         <div class="col-md-3 mb-3">
-                          <label class="form-label" title="Mínimo 200px x 100px em formato JPG"><?=chave('carimbo')?> <small>(200px X 100px JPG)</small></label>
+                          <label class="form-label" title="Mínimo 200px x 100px em formato JPG"><?= chave('carimbo') ?> <small>(200px X 100px JPG)</small></label>
                           <input class="form-control input-carimbo" type="file" value="<?= $certificado['carimbo'] ?? ''; ?>">
-                          
+
                         </div>
 
                         <div class="col-md-3 mb-3">
-                          <label class="form-label" title="Mínimo 200px x 100px em formato JPG"><?=chave('assinatura')?> <small>(200px X 100px JPG)</small></label>
+                          <label class="form-label" title="Mínimo 200px x 100px em formato JPG"><?= chave('assinatura') ?> <small>(200px X 100px JPG)</small></label>
                           <input class="form-control input-assinatura" type="file" value="<?= $certificado['assinatura'] ?? ''; ?>">
-                          
+
                         </div>
 
                         <div class="col-md-3 mb-3">
-                          <label class="form-label" title="Mínimo 200px x 100px em formato JPG"><?=chave('marcadagua')?> <small>(200px X 100px JPG)</small></label>
+                          <label class="form-label" title="Mínimo 200px x 100px em formato JPG"><?= chave('marcadagua') ?> <small>(200px X 100px JPG)</small></label>
                           <input class="form-control input-marca-agua" type="file" value="<?= $certificado['marca_agua'] ?? ''; ?>">
-                          
+
                         </div>
 
                         <div class="flex-1 text-end my-5">
