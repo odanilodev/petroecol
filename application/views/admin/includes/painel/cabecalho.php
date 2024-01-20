15340da16d836ea790aeab99bd7f71cde81e9bc3
Victor Hugo Prado <97893980+ovitindev@users.noreply.github.com>
2024-01-20 09:58:03 -0300

    Adicionar link para o admin na logo do sistema (#221)

--- application/views/admin/includes/painel/cabecalho.php
+++ application/views/admin/includes/painel/cabecalho.php
@@ -146,7 +146,7 @@
                 <div class="navbar-logo">
 
                     <button class="btn navbar-toggler navbar-toggler-humburger-icon hover-bg-transparent" type="button" data-bs-toggle="collapse" data-bs-target="#navbarVerticalCollapse" aria-controls="navbarVerticalCollapse" aria-expanded="false" aria-label="Toggle Navigation"><span class="navbar-toggle-icon"><span class="toggle-line"></span></span></button>
-                    <a class="navbar-brand me-1 me-sm-3" href="<?= base_url('dashboard') ?>">
+                    <a class="navbar-brand me-1 me-sm-3" href="<?= base_url('admin') ?>">
                         <div class="d-flex align-items-center">
                             <div class="d-flex align-items-center">
                                 <img src="" style="max-width: 180px;" class="img-fluid logo" />
