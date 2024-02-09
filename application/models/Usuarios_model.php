58676829c40ca246400445477ab40d84f5fe6f76
Guilherme Prado <guilherme6788@hotmail.com>
2024-02-09 10:44:13 +0000

    Exclusão de código comentado

--- application/models/Usuarios_model.php
+++ application/models/Usuarios_model.php
@@ -152,16 +152,7 @@ class Usuarios_model extends CI_Model
 
         return $this->db->affected_rows() > 0;
     }
-
-    // public function verificaSetorUsuario($id)
-    // {
-    //     $this->db->where('id_setor', $id);
-    //     $this->db->where('id_empresa', $this->session->userdata('id_empresa'));
-    //     $this->db->get('ci_usuarios');
-
-    //     return $this->db->affected_rows() > 0;
-    // }
-
+    
     public function verificaSetorUsuario($id)
     {
         $this->db->select('U.id_setor, GROUP_CONCAT(DISTINCT S.nome) as nomes_SETORES');
