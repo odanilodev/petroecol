a2eae11f72144841cd66bcee5105b77347f6fcae
oluisfelipedev <lipe@centrodainteligencia.com.br>
2024-03-14 12:02:22 -0300

    corrigir filtro de clientes no romaneio

--- assets/js/romaneios/romaneio.js
+++ assets/js/romaneios/romaneio.js
@@ -182,16 +182,10 @@ const gerarRomaneio = () => {
     });
 
     // novo clientes para gerar romaneio
-    let clientes = [];
-    $('.check-clientes-modal').each(function () {
+    let clientes =$('.ids-selecionados').val().split(',');;
+    console.log(clientes)
 
-        if ($(this).is(':checked')) {
-
-            clientes.push($(this).val());
-
-        }
-
-    })
+    
 
     if (clientes.length < 1) {
         avisoRetorno('Algo deu errado', 'Você precisa selecionar algum cliente para gerar o romaneio.', 'error', '#');
