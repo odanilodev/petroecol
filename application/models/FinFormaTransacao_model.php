9c2ce5ddc34c1c20b4c2739cd16ddb8d01075d4d
Guilherme B. Prado <149012697+guilhermebprd-dev@users.noreply.github.com>
2024-03-23 12:03:11 -0300

    Update FinFormaTransacao_model.php
    Carregamento correto do model

--- application/models/FinFormaTransacao_model.php
+++ application/models/FinFormaTransacao_model.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class FinMacro_model extends CI_Model
+class FinFormaTransacao_model extends CI_Model
 {
 
     public function __construct()
