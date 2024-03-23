c07e3d5ca08175ccd050152dfc5aaa5071043b96
Luis Felipe Lopes <99437129+oluisfelipedev@users.noreply.github.com>
2024-03-23 10:55:25 -0300

    front da pagina de fluxo de caixa (#302)
    * front da pagina de fluxo de caixa
    
    * alterações
    
    * correcoes e js

--- application/helpers/template_helper.php
+++ application/helpers/template_helper.php
@@ -448,3 +448,12 @@ function scriptsFinMacroFooter()
     );
 }
 
+// Pagina de fluxo de caixa financeiro
+function scriptsFinFluxoFooter()
+{
+    return array(
+
+        '<script src="' . base_url('assets/js/financeiro/fluxo-caixa/fluxo-caixa.js') . '"></script>'
+
+    );
+}
