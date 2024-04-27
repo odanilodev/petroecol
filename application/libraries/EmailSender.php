b8272b7ce01bda811ed043cb1297f69ba81cf7b3
Danilo <contato@centrodainteligencia.com.br>
2024-04-27 14:41:34 -0300

    Debugar

--- application/libraries/EmailSender.php
+++ application/libraries/EmailSender.php
@@ -21,6 +21,7 @@ class EmailSender
                 break;
             case 'enviarCertificado':
                 $html = $this->enviarCertificado();
+                var_dump($opcao);
                 $this->CI->email->attach($opcao, 'certificado.pdf', 'application/pdf');
                 break;
             default:
