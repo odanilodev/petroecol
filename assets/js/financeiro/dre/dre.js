8dd35d5c8d42cefdce992bc2c30151e9200f3461
oluisfelipedev <lipe@centrodainteligencia.com.br>
2024-05-21 17:44:52 -0300

    porcentagem DRE

--- assets/js/financeiro/dre/dre.js
+++ assets/js/financeiro/dre/dre.js
@@ -25,6 +25,7 @@ function buscaValoresMicros(idMicro) {
     $.ajax({
         type: "post",
         url: `${baseUrl}finDre/recebeValoresMicros`,
+        async: true,
         data: {
             idMicro: idMicro
         }, beforeSend: function () {
@@ -33,13 +34,11 @@ function buscaValoresMicros(idMicro) {
 
             let valorFaturamento = $('.valor-faturamento').val(); 
 
-            let micros = '';
-
             for (i = 0; i < data['retorno'].length; i++) {
 
                 let valor = formatarValorMoeda(data['retorno'][i].VALOR_TOTAL_MICRO);
 
-                let porcentagemFatura = (data['retorno'][i].VALOR_TOTAL_MICRO - valorFaturamento) / valorFaturamento * 100;
+                let porcentagemFatura = (valorFaturamento - data['retorno'][i].VALOR_TOTAL_MICRO) / valorFaturamento * 100;
                 porcentagemFatura = 100 - porcentagemFatura;
 
                 let porcentagemFaturaFormatada = porcentagemFatura.toLocaleString('pt-BR', {
@@ -47,16 +46,16 @@ function buscaValoresMicros(idMicro) {
                     maximumFractionDigits: 2
                 }) + '%';
 
-                micros += `
+                let micros = `
                     <tr class="html-clean">
                         <td class="text-center nomes-micros html-clean">${data['retorno'][i].nome}</td>
                         <td class="text-center valor-micro">${valor}</td>
                         <td class="text-center">${porcentagemFaturaFormatada}</td>
                     </tr>
                 `;
+                $('.tabela-micros').append(micros);
             }
 
-            $('.tabela-micros').append(micros);
 
         }
     })
