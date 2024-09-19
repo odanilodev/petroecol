ac2c9649911a19d51c8ae0024a6ab453649cec16
oluisfelipedev <lipe@centrodainteligencia.com.br>
2024-09-19 13:43:06 -0300

    adicionar idSetor no exportar excel

--- application/controllers/FinFluxoCaixa.php
+++ application/controllers/FinFluxoCaixa.php
@@ -288,9 +288,12 @@ class FinFluxoCaixa extends CI_Controller
         $dataInicio = $this->input->post('data_inicio');
         $dataFim = $this->input->post('data_fim');
         $tipoMovimentacao = $this->input->post('movimentacao');
+        $idSetor = $this->input->post('setor-empresa');
+
 
         // Define valores padrão caso os filtros não sejam fornecidos
         $tipoMovimentacao = $tipoMovimentacao ? $tipoMovimentacao : 'ambas';
+        $idSetorEmpresa = $idSetor ? $idSetor : 'todos';
 
         // Formata as datas para o formato Y-m-d
         $dataInicioFormatada = date('Y-m-d', strtotime(str_replace('/', '-', $dataInicio)));
@@ -298,7 +301,7 @@ class FinFluxoCaixa extends CI_Controller
 
         // Carrega o modelo e obtém os dados filtrados
         $this->load->model('FinFluxo_model');
-        $movimentacoes = $this->FinFluxo_model->recebeFluxoData($dataInicioFormatada, $dataFimFormatada, $tipoMovimentacao);
+        $movimentacoes = $this->FinFluxo_model->recebeFluxoData($dataInicioFormatada, $dataFimFormatada, $tipoMovimentacao, $idSetorEmpresa);
 
         // Cria o conteúdo do Excel usando XML
         $xml = '<?xml version="1.0" encoding="UTF-8"?>';
