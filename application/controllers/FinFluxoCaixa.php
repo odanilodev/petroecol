42cc8bd33787bb5a9148828d0a5c9f8512a5912b
oluisfelipedev <lipe@centrodainteligencia.com.br>
2024-10-15 16:14:24 -0300

    alterar data do fluxo ao adicionar verbas ao responsavel

--- application/controllers/FinFluxoCaixa.php
+++ application/controllers/FinFluxoCaixa.php
@@ -176,7 +176,7 @@ class FinFluxoCaixa extends CI_Controller
         $idResponsavel = $this->input->post('responsavel');
         $idEmpresa = $this->input->post('setorEmpresa');
         $codigoRomaneio = $this->input->post('codRomaneio');
-
+        $dataRomaneio = $this->input->post('dataRomaneio');
         $this->load->model('Funcionarios_model');
 
         $dadosFluxo = $this->input->post('dadosFluxo');
@@ -195,7 +195,7 @@ class FinFluxoCaixa extends CI_Controller
 
                 $dados['id_micro'] = $dadosFluxo['id_micro'][$i];
                 $dados['id_macro'] = $dadosFluxo['id_macro'][$i];
-                $dados['data_movimentacao'] = date('Y-m-d');
+                $dados['data_movimentacao'] = $dataRomaneio;
                 $dados['id_funcionario'] = $idResponsavel;
                 $dados['id_setor_empresa'] = $idEmpresa;
                 $dados['observacao'] = "Romaneio: $codigoRomaneio";
