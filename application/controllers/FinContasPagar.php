cda6608df6353188504e6e5e4961c3954f82c464
Luis Felipe Lopes <99437129+oluisfelipedev@users.noreply.github.com>
2024-11-12 10:42:23 -0300

    resolver problema de data ao editar uma conta (#587)

--- application/controllers/FinContasPagar.php
+++ application/controllers/FinContasPagar.php
@@ -280,10 +280,10 @@ class FinContasPagar extends CI_Controller
 			$data['id_dado_financeiro'] = $dadosLancamento['recebido'];
 		}
 
-		$data['valor'] = str_replace(['.', ','], ['', '.'], $dadosLancamento['valor']);
+		$data['valor'] = str_replace(['.', ','], ['', '.'], $dadosLancamento['valor'][0]);
 		$data['observacao'] = $dadosLancamento['observacao'];
 		$data['id_setor_empresa'] = $dadosLancamento['setor'];
-		$data['data_vencimento'] = date('Y-m-d', strtotime(str_replace('/', '-', $dadosLancamento['data_vencimento'])));
+		$data['data_vencimento'] = date('Y-m-d', strtotime(str_replace('/', '-', $dadosLancamento['data_vencimento'][0])));
 		$data['data_emissao'] = $dadosLancamento['data_emissao'] ? date('Y-m-d', strtotime(str_replace('/', '-', $dadosLancamento['data_emissao']))) : "";
 		$data['id_empresa'] = $this->session->userdata('id_empresa');
 		$data['id_micro'] = $dadosLancamento['micros'];
