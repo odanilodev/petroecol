1449afe49fec059f59473076a8b330f718c64bba
oluisfelipedev <lipe@centrodainteligencia.com.br>
2024-12-12 16:16:28 -0300

    corrigir observacao da proxima coleta do cliente

--- application/models/Clientes_model.php
+++ application/models/Clientes_model.php
@@ -415,4 +415,14 @@ class Clientes_model extends CI_Model
 
         return $this->db->affected_rows() > 0;
     }
+
+    public function recebeDataObservacaoColetaCliente($idCliente)
+    {
+        $this->db->select('data_observacao_coleta');
+        $this->db->where('id_empresa', $this->session->userdata('id_empresa'));
+        $this->db->where('id', $idCliente);
+        $query = $this->db->get('ci_clientes');
+        return $query->row_array();
+    }
+
 }
